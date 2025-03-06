101ab480ce766d1e863810eab2e5a178003f5d39
Sheila Samita <samitasheila1@gmail.com>
2025-03-07 00:56:48 +0300

    arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100) + 1;
+let num3 = 28;
+let num4 = 6;
+let mod = num3 % num4; // 28 % 6 = 4
+let max = Math.max(1, 5, 10, 15, 20);
+console.log(multiply, random, mod, max);
